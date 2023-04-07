b1ddcc46b7cf25f3f4cec56f5ecc14728de4d577
Henry Le <104451202+Henry-Le-CS@users.noreply.github.com>
2023-04-07 07:17:06 +0700

    upload: push the most updated files of backend

---
+++ Modelling/be/routes/boRouter.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const boController = require("../controllers/boControllers");
+const app = express();
+const router = express.Router();
+
+router.get("/viewMCP", boController.viewMCP);
+router.get("/viewWorker", boController.viewWorker)
+router.get("/viewGroup",boController.viewGroup);
+router.get("/viewVehicle",boController.viewVehicle)
+router.post("/findGroup",boController.findGroup)
+router.post("/infoBO", boController.infoBO);
+router.post("/assignUser",boController.assignUser);
+router.post("/assignVehicle",boController.assignVehicle);
+router.post("/removeGroup",boController.removeGroup);
+router.post("/findWorker",boController.findWorker)
+router.post("/listInfo",boController.listInfo)
+router.post("/writeDescription",boController.writeDescription)
+module.exports = router;
---
+++ Modelling/be/routes/userRouter.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const userController = require("../controllers/userControllers");
+const app = express();
+const router = express.Router();
+
+router.post("/login",userController.login);
+
+
+module.exports = router;
