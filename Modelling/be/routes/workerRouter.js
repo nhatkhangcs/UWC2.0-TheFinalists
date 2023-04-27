185ee21ba475f3787a8f0c773796cb26c491b5cb
Võ Hoàng Nhật Khang <khang.vo872003@hcmut.edu.vn>
2023-04-28 00:41:34 +0700

    Update workerRouter.js

--- Modelling/be/routes/workerRouter.js
+++ Modelling/be/routes/workerRouter.js
@@ -3,4 +3,6 @@ const workerController = require("../controllers/workerControllers");
 const app = express();
 const router = express.Router();
 router.post("/findGroups",workerController.findGroups)
+router.post("/listInfoTasks",workerController.listInfo)
+router.post("/markStatus", workerController.markStatus)
 module.exports = router;
